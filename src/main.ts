82dd73fbdef0485d992aa3f571664abd71e89ec8
simooowann <simooowann@gmail.com>
2023-11-30 19:40:05 +0800

    删除无用依赖

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
